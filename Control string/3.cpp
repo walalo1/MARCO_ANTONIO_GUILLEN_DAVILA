8dfefb82165d99788e466029493b4c393b774317
walalo1 <81270942+walalo1@users.noreply.github.com>
2021-04-16 13:43:44 -0500

    Create 3.cpp

---
+++ Control string/3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int numero;
+  cout << "Ingrese numero: ";
+  cin >> numero;
+  string numero1 = to_string(numero);
+  cout << numero1 << endl;
+  return 0;
+}
+
+
