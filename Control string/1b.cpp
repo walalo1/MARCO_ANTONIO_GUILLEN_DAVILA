42aeccd47bdbc89191e1628d6c86d0995cb777ff
walalo1 <81270942+walalo1@users.noreply.github.com>
2021-04-16 13:41:14 -0500

    Create 1b.cpp

---
+++ Control string/1b.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string cadena;
+  int espacio;
+  cout << "Ingrese su texto: ";
+  getline(cin, cadena);
+  cout << "\ningrese el tamaño de los bloques: ";
+  cin >> espacio;
+  return 0;
+}
